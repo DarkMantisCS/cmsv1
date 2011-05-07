33f7ee727387ab204c1343d77cf5b60b64a627ff
xLink <xlink@cybershade.org>
2011-05-07 05:22:23 +0100

    added core & form classes

---
+++ core/classes/class.core.php
@@ -0,0 +1,48 @@
+<?php
+/*======================================================================*\
+||              Cybershade CMS - Your CMS, Your Way                     ||
+\*======================================================================*/
+if(!defined('INDEX_CHECK')){ die('Error: Cannot access directly.'); }
+
+/**
+ * Group Class designed to allow easier access to expand on the group system implemented
+ *
+ * @version     1.0
+ * @since       1.0.0
+ * @author      xLink
+ */
+abstract class Core {
+
+    /**
+     * Sets a variable with a value
+     *
+     * @version	1.0
+     * @since   1.0.0
+     * @author  xLink
+     *
+     * @param   string 	$var
+     * @param   mixed	$value
+     *
+     * @return  null
+     */
+	public function setVar($var, $value){
+		$this->$var = $value;
+	}
+
+    /**
+     * Returns a var's value
+     *
+     * @version	1.0
+     * @since   1.0.0
+     * @author  xLink
+     *
+     * @param   string 	$var
+     *
+     * @return  mixed
+     */
+	public function getVar($var){
+		return (isset($this->$var) ? $this->$var : false);
+	}
+
+}
+?>
