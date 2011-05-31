3b3f17ac6dec882848cfa0ca4664b0917f32b29f
Dan Aldridge <xLink@cybershade.org>
2011-05-31 15:37:59 +0100

    added gritter for the notification sys

--- scripts/config.php
+++ scripts/config.php
@@ -1,5 +1,5 @@
 <?php
 $scripts = array();
 $scripts['framework'] = array('jquery-1.6.1.js', 'jqueryui-1.8.12.js', 'modernizr.js');
-$scripts['extras'] = array('vertigro.js', 'autoselect.js', 'cookie.js', 'tabable.js', 'colorbox.js', 'core.js', 'adapt.js');
+$scripts['extras'] = array('gritter.js', 'vertigro.js', 'autoselect.js', 'cookie.js', 'tabable.js', 'colorbox.js', 'core.js', 'adapt.js');
 ?>
