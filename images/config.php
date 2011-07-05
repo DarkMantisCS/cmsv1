98277d5b6d9019470392b017645b338332f7454a
Dan Aldridge <xLink@cybershade.org>
2011-07-05 22:48:53 +0100

    added forgotten files

--- images/config.php
+++ images/config.php
@@ -1,5 +1,5 @@
 <?php
 $styles = array();
 $styles['framework'] = array('reset.css', 'default.css', 'general.css', 'forms.css');
-$styles['extras'] = array('jqueryui-1.8.12.css', 'buttons.css', 'gritter.css', 'selectBox.css', 'contextmenu.css');
+$styles['extras'] = array('buttons.css', 'contextmenu.css', 'lightwindow.css');
 ?>
